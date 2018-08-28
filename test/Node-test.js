6edd2f54cb91096a4d4b094f1f6dd23ba93dfe71
Alexander Ela <ela.alexander@gmail.com>
2018-08-28 13:39:59 -0600

    Add first Node class tests

--- test/Node-test.js
+++ test/Node-test.js
@@ -1,25 +1,21 @@
-const { assert, expect } = require('chai');
-const Trie = require('../lib/Trie.js');
-const Node = require('../lib/Node.js');
-let node;
+import { expect } from 'chai';
+import Node from '../lib/Node';
 
-beforeEach(() => {
-	node = new Node;
-});
-
-// Phase 1
-
-describe('Node reference', function() {
-	it('should have a property that references other nodes', function() {
-		assert.equal(node.next, null);
-	});
-});
-
-
-describe('Data representation', function() {
-
-	it('should have a property that represents data', function() {
-			const node = new Node('hello')
-			expect(node.data).to.be.a('string');
-	});
+describe('NODE', () => {
+  let node;
+  beforeEach(() => {
+    node = new Node('p');
+  })
+  it('should exist', () => {
+    expect(node).to.exist;
+  })
+  it('should take letter as argument and assign it to the letter property', () => {
+    expect(node.letter).to.equal('p');
+  })
+  it('should default end to false', () => {
+    expect(node.end).to.equal(false)
+  })
+  it('should default children to an empty object', () => {
+    expect(node.children).to.deep.equal({});
+  })
 });
