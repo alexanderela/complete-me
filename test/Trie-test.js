09201bc0ebc466cb8abfb40af7f343d9710c257f
Alexander Ela <ela.alexander@gmail.com>
2018-08-29 14:42:13 -0600

    Add insert and suggest methods

--- test/Trie-test.js
+++ test/Trie-test.js
@@ -1,42 +1,119 @@
 import { expect } from 'chai';
 import Trie from '../lib/Trie'
 import Node from '../lib/Node'
-
-describe('TRIE', () => {
-  let trie;
+let trie;
 
   beforeEach(() => {
     trie = new Trie();
   });
 
-  it('should exist', () => {
-    expect(trie).to.exist;
-  })
-
-  it('should start with zero elements', () => {
-    expect(trie.totalWords).to.equal(0);
+describe('test', () => {
+  it('should return true', () => {
+    expect(true, true);
   });
-
-  it('should set its default root to empty object', () => {
-    expect(trie.root.children).to.deep.eq({});
-  });
-
-  it('should increase totalWords each time we instantiate a new word', () => {
-    expect(trie.totalWords).to.eq(0);
-    trie.insert('word')
-    expect(trie.totalWords).to.eq(1)
-  });
-
-it ('should insert word correctly when calling insert', () => {
-    trie.insert ('hello');
-    trie.insert ('cool')
-    trie.insert ('loop')
-    // console.log(JSON.stringify(trie, null, 4))
-    expect(Object.keys(trie.root.children)).to.deep.eq([ 'h', 'c', 'l' ]);
-  });
-
-
  });
+
+
+describe('count', () => {
+	it('should increment wordcount by 1 each time', () => {
+		trie.count();
+		trie.count();
+		expect(trie.totalWords, 2);
+	});
+});
+
+
+describe('insert', () => {
+	it('should increment wordcount by 1 each time', () => {
+		trie.insert('sassfactory');
+		trie.insert('detroit');
+		expect(trie.totalWords, 2);
+	});
+});
+
+describe('insert recursive', () => {
+	it('should return a word', () => {
+	
+	})
+
+	it('should insert a word when invoking insert', () => {
+		trie.insert('hello');
+		trie.insert('cool');
+		trie.insert('popeyes');
+		console.log(JSON.stringify(trie.root, null, 4));
+		expect(Object.keys(trie.root.children)).to.deep.equal(['h', 'c', 'p']);
+	})
+
+
+	it('should check child', () => {
+		trie.insert('hello');
+		trie.insert('hellen');
+
+
+		// console.log(JSON.stringify(trie.root, null, 4));
+		// console.log(Object.keys(trie.root.children));
+		// expect(Object.keys(trie.root.children)).to.deep.equal(['h', 'c', 'p']);
+	})
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// describe('TRIE', () => {
+//   let trie;
+
+//   beforeEach(() => {
+//     trie = new Trie();
+//   });
+
+//   it('should exist', () => {
+//     expect(trie).to.exist;
+//   })
+
+//   it('should start with zero elements', () => {
+//     expect(trie.totalWords).to.equal(0);
+//   });
+
+//   it('should set its default root to empty object', () => {
+//     expect(trie.root.children).to.deep.eq({});
+//   });
+
+//   it('should increase totalWords each time we instantiate a new word', () => {
+//     expect(trie.totalWords).to.eq(0);
+//     trie.insert('word')
+//     expect(trie.totalWords).to.eq(1)
+//   });
+
+// it ('should create a new branch for each unique first letter', () => {
+//     trie.insert ('hello');
+//     trie.insert ('cool')
+//     trie.insert ('loop')
+//     // console.log(JSON.stringify(trie, null, 4))
+//     expect(Object.keys(trie.root.children)).to.deep.eq([ 'h', 'c', 'l' ]);
+//   });
+
+
+//  });
 
 
 
