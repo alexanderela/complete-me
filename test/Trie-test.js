2deb63f4bfd779bb91ead27e271c4b3f49ad8569
Alexander Ela <ela.alexander@gmail.com>
2018-08-29 22:22:33 -0600

    Add functional test for dictionary

--- test/Trie-test.js
+++ test/Trie-test.js
@@ -29,7 +29,6 @@ describe('Insert', () => {
 		trie.insert('hello');
 		trie.insert('cool');
 		trie.insert('popeyes');
-		// console.log(JSON.stringify(trie.root, null, 4));
 		expect(Object.keys(trie.root.children)).to.deep.equal(['h', 'c', 'p']);
 	});
 });
@@ -43,157 +42,13 @@ describe('Suggest', () => {
 		trie.insert('hexagon');
 
 		expect(trie.suggest('he')).to.deep.equal(['hello', 'hellen', 'hendrix', 'henderson', 'hexagon']);
-		// console.log(trie.suggest('he'))
-		// console.log(JSON.stringify(currentNode, null, 4));
-		// console.log(Object.keys(trie.root.children));
-
 	});
 });
 
 describe('Populate', () => {
-	it('should return an array of word suggestions from dictionary database', () => {
+	it('should populate trie with dictionary dataset', () => {
+		expect(trie.totalWords).to.equal(0);
 		trie.populate(dictionary);
-		console.log(trie.count())
-		// expect(trie.suggest('wo')).to.deep.equal([ 'world', 'worlded', 'worldful', 'worldish'])
-	it('should return an array of word suggestions from dictionary database', () => {
-		
-		trie.populate(dictionary);
+		expect(trie.totalWords).to.equal(235886);
 	});
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// describe('TRIE', () => {
-//   let trie;
-
-//   beforeEach(() => {
-//     trie = new Trie();
-//   });
-
-//   it('should exist', () => {
-//     expect(trie).to.exist;
-//   })
-
-//   it('should start with zero elements', () => {
-//     expect(trie.totalWords).to.equal(0);
-//   });
-
-//   it('should set its default root to empty object', () => {
-//     expect(trie.root.children).to.deep.eq({});
-//   });
-
-//   it('should increase totalWords each time we instantiate a new word', () => {
-//     expect(trie.totalWords).to.eq(0);
-//     trie.insert('word')
-//     expect(trie.totalWords).to.eq(1)
-//   });
-
-// it ('should create a new branch for each unique first letter', () => {
-//     trie.insert ('hello');
-//     trie.insert ('cool')
-//     trie.insert ('loop')
-//     // console.log(JSON.stringify(trie, null, 4))
-//     expect(Object.keys(trie.root.children)).to.deep.eq([ 'h', 'c', 'l' ]);
-//   });
-
-
-//  });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const { assert } = require('chai');
-// const Trie = require('../lib/Trie.js');
-// const Node = require('../lib/Node.js');
-// let trie;
-
-    
-// beforeEach(() => {
-// 	trie = new Trie;
-// });
-
-// // Phase 1
-
-//  describe('test', function() {
-//    it('should return true', function() {
-//      assert.equal(true, true);
-//    });
-
-//  describe('count', function() {
-//  		it('should keep count of how many words have been inserted', function(){
-//  			trie.insert('word1');
-//  			trie.insert('word2')
-//  			assert.equal(trie.totalWords, 2);
-//  		});
-//  });
-
-//  describe('insert', function() {
-//  		it('should take a string as a parameter', function() {
-//  			trie.insert('word');
-//  			assert.isNotNull(trie.head);
-//  		});
-//  });
-
-// // Phase 2
-
-// 	describe('', function() {
-// 		it('should', function() {
-
-// 		});
-// 	});
-
-// 	describe('', function() {
-// 	it('should', function() {
-
-// 		});
-// 	});
-
-
-
-
-//  });
